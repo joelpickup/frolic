d7b7a82776e8598050aa307ad936ac33125c955e
Joel Pickup <joelpickup@hotmail.co.uk>
2015-02-12 18:22:03 +0000

    started the new meetup form

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def show
+    @my_meetups = current_user.meetups
     render 'dashboard'
   end
 end
---
+++ app/controllers/meetups_controller.rb
@@ -0,0 +1,6 @@
+class MeetupsController < ApplicationController
+def create
+  puts "*"*1000
+  redirect_to dashboard_path
+end
+end
