64926cbfae14edc7ba4bcc3013301193037269fa
Joel Pickup <joelpickup@hotmail.co.uk>
2015-02-16 11:40:27 +0000

    got searchbox on the map which loads the google places api, need to get it to move the map to the location supplied

--- app/assets/javascripts/meetup_show.js
+++ app/assets/javascripts/meetup_show.js
@@ -5,13 +5,68 @@ myMap.initialize = function() {
  var mapOptions = {
   center: { lat:  51.52, lng: -0.115},
   zoom: 14,
-           mapTypeId:google.maps.MapTypeId.ROADMAP //default
-         };
+  mapTypeId:google.maps.MapTypeId.ROADMAP //default
+  };
 
-         var map = new google.maps.Map(myMap.mapCanvas, mapOptions);
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var input = /** @type {HTMLInputElement} */(
+        document.getElementById('pac-input'));
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-       };
-       $(function(){
-         myMap.mapCanvas = $('#map-canvas')[0];
-         myMap.initialize();
-       });
+    var searchBox = new google.maps.places.SearchBox(
+      /** @type {HTMLInputElement} */(input));
+
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+      for (var i = 0, marker; marker = markers[i]; i++) {
+        marker.setMap(null);
+      }
+
+      // For each place, get the icon, place name, and location.
+      markers = [];
+      var bounds = new google.maps.LatLngBounds();
+      for (var i = 0, place; place = places[i]; i++) {
+        var image = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        var marker = new google.maps.Marker({
+          map: map,
+          icon: image,
+          title: place.name,
+          position: place.geometry.location
+        });
+
+        markers.push(marker);
+
+        bounds.extend(place.geometry.location);
+      }
+
+      map.fitBounds(bounds);
+    });
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+      var bounds = map.getBounds();
+      searchBox.setBounds(bounds);
+    });
+ 
+
+};
+
+$(function(){
+ myMap.mapCanvas = $('#map-canvas')[0];
+ myMap.initialize();
+});
