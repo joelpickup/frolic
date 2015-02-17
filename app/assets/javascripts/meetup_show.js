b1aa4d55e4e2cc4f9506d4dbe9c5552c740e0034
Joel Pickup <joelpickup@hotmail.co.uk>
2015-02-17 16:42:33 +0000

    can finally a venue_suggestion form generating with dynamic data from the places API, now need to ajax the form so it can send off, then show on the show page when one is created

--- app/assets/javascripts/meetup_show.js
+++ app/assets/javascripts/meetup_show.js
@@ -42,94 +42,50 @@ myMap.initialize = function() {
         map: map,
         icon: image,
         title: place.name,
-        position: place.geometry.location
+        position: place.geometry.location,
+        id: place.id,
+        popup: new google.maps.InfoWindow({
+          content: place.name + "<button data-name='"+place.name+"' data-lat='"+place.geometry.location.k+"' data-long='"+place.geometry.location.D+"' id='"+place.id+"'>Click!</button>"
+        })
+      });
+
+      $("#map-canvas").on('click', '#'+ place.id, function(event){
+        event.preventDefault();
+        data = this.dataset;
+        id = $('#meetup_id').html();
+        form = "<div id='suggestion_"+id+"'><form action='/meetups/"+id+"/venue_suggestions/' data-remote='true'><input type='text' name='venue_suggestion[venue_name]' value='"+data.name+"'><input type='hidden' name='venue_suggestion[lat]' value='"+data.lat+"'><input type='hidden' name='venue_suggestion[long]' value='"+data.long+"'><input type='text' name='venue_suggestions[event_name]' placeholder='Event Name'><textarea name='venue_suggestion[event_description]' placeholder='Event Description'></textarea><input type='submit' value='suggest!'><a href='#' id='cancel'>cancel</a></div>";
+        $('#suggestions_header').append(form);
+        $('#cancel').on("click", function(){
+          $('#suggestion_'+id).remove();
+        });
+      });
+
+      // Add Listener to each marker, defining each 
+      google.maps.event.addListener(marker, 'click', function(){
+        this.popup.open(map, this);
       });
 
       
       markers.push(marker);
       bounds.extend(place.geometry.location);
       event_centre = place.geometry.location;
-      google.maps.event.addListener(marker, 'click', function(){
-        id = $('#meetup_id').html();
-        var popupOptions = {
-          content: this.title + "<form action='/meetups/"+id+"/venue_suggestions/new' data-remote='true'><input type='hidden' name='venue_suggestion[venue_name]' value='"+this.title+"'><input type='hidden' name='venue_suggestion[meetup_id]' value='"+id+"'><input type='hidden' name='venue_suggestion[lat]' value='"+this.position.k+"'><input type='hidden' name='venue_suggestion[long]' value='"+this.position.D+"'><button>Suggest this!</button></form>"
-        };
-        var popup = new google.maps.InfoWindow(popupOptions);
-        
-        popup.open(map, this);
-      });
     }
     map.fitBounds(bounds);
   });
+
+  
 
 // Bias the SearchBox results towards places that are within the bounds of the
 // current map's viewport.
 google.maps.event.addListener(map, 'bounds_changed', function() {
   var bounds = map.getBounds();
   searchBox.setBounds(bounds);
-});
-
-
-
+  });
 };
+
+
 
 $(function(){
  myMap.mapCanvas = $('#map-canvas')[0];
  myMap.initialize();
- $('#event_search').on('click', function(){
-  latitude = String(event_centre.k);
-  longitude = String(event_centre.D);
-  $.getJSON("https://www.eventbriteapi.com/v3/events/search/?location.within=2km&location.latitude="+ latitude + "&location.longitude="+longitude+"&categories=103%2C110&start_date.keyword=this_week&token=SHSAVN36MVXA7GYU7G5P", function(data) {
-    console.log(data.events);
-    var markers = [];
-
-    var events = data.events;
-    console.log(events);
-    if (events.length == 0) {
-      return;
-    }
-    for (var i = 0, marker; marker = markers[i]; i++) {
-      marker.setMap(null);
-    }
-
-    markers = [];
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0, event; event = events[i]; i++) {
-      var image = {
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      var marker = new google.maps.Marker({
-        map: global_map,
-        title: event.name.text,
-        position: new google.maps.LatLng(event.venue.latitude, event.venue.longitude),
-        id: event.id,
-        logo: event.logo_url,
-        url: event.url,
-        venue_id: event.venue_id,
-        time: event.start.local,
-        event_id: event.id,
-        category: event.category.name
-      });
-
-      
-      markers.push(marker);
-      bounds.extend(new google.maps.LatLng(event.venue.latitude, event.venue.longitude));
-      google.maps.event.addListener(marker, 'click', function(){
-        var popupOptions = {
-          content: "<h2><strong>Name:</strong>"+this.title+"</h2><br><p class='time'><strong>Time:</strong>"+this.time+"</p><p class='category'><strong>Category:</strong>"+this.category+"</p><img style='height: 20px; width: 20px;'src='"+this.logo+"' class=''alt=''><a href='"+this.url+"' target='_blank'>More info</a><button>suggest this!</button>"
-
-        };
-        var popup = new google.maps.InfoWindow(popupOptions);
-        popup.open(global_map, this);
-      });
-
-    }
-    global_map.fitBounds(bounds);
-  });
-});
 });
