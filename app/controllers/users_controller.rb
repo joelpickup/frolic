098cd146be21d64a50a866db3f300d2fde100075
Joel Pickup <joelpickup@hotmail.co.uk>
2015-03-04 18:29:56 +0000

    removed unneccessary action from controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,6 @@
 class UsersController < ApplicationController
   respond_to :html, :xml, :json
 
-  def load_users
-    @users = User.all
-    @users_json = []
-    @users.each {|u| @users_json.push({"id" => u["id"], "first_name" => u["first_name"], "surname" => u["surname"]})}
-    render json: @users_json
-  end
-
   def make_home
     current_user.home_id = params[:id]
     redirect_to edit_user_registration_path
